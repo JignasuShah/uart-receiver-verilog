65425cbc2acf96df516c05ac14c2ebc3670b3512
JignasuShah <jss508@scarletmail.rutgers.edu>
2026-08-26 16:11:48 -0400

    Added initial timing information

--- tb/uart_rx_tb.sv
+++ tb/uart_rx_tb.sv
@@ -11,5 +11,14 @@ wire rx_out;
 
 uart_rx_top #(.CLK_FREQ(50000000), .BAUD_RATE(115200)) dut (.clk(clk), .rst(rst), .rx_in(rx_in), .rx_valid(rx_valid), .rx_out(rx_out)); 
 
+initial clk = 1'b0; 
+always #10 clk = ~clk; 
+
+initial begin
+    rst = 1'b1; 
+    rx_in = 1'b1; 
+    #40; 
+    rst = 1'b0; 
+end
 
 endmodule 
